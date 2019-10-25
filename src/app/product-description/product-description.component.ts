e466571c6dba4f99e7492cba300d5392a455a957
Joseph Omitogun <joseyi2013@gmail.com>
2019-10-25 17:28:11 +0100

    updated  files

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -9,10 +9,10 @@ import {ProductService} from '../product.service';
 export class ProductDescriptionComponent implements OnInit {
   albumInfo:any;
 
-  constructor(private _productSevice:ProductService) { }
+  constructor(private _productService:ProductService) { }
 
   ngOnInit() {
-    this._productSevice.getAlbum(1).subscribe(response=>this.albumInfo=response);
+    this._productService.getAlbum(1).subscribe(response=>this.albumInfo=response);
   }
 
 }
